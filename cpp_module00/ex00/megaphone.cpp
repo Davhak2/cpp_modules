5245d0239543adb1e5ea7dc842780045f8d39315
Davhak2 <davidhakobyan014@gmail.com>
2025-10-08 02:37:46 +0400

    ex00 done

---
+++ cpp_module00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void	print_str(std::string s)
+{
+	for (size_t i = 0; i < s.length(); i++)
+		std::cout << (char)toupper(s[i]);
+}
+
+int	main(int argc, char **argv){
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else {
+		for (int i = 1; i < argc; i++)
+			print_str(argv[i]);
+		std::cout << std::endl;
+	}
+	return 0;
+}
